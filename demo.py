0515c729155fef8be490b804e93b8d131281f3d9
mohfarahh <farahmohamed542@gmail.com>
2023-10-05 17:03:55 -0500

    Created my first git

---
+++ demo.py
@@ -0,0 +1,21 @@
+def findMaxOfArray(array):
+    max =  array[0]
+    for i in range(len(array)-1):
+        if array[i] > max:
+            max = array[i]
+            
+    return max
+
+
+def findMinOfArray(array):
+    min = array[0]
+    for i in range(len(array)-1):
+        if array[i] < min:
+            min = array[i]
+            
+    return min
+
+
+
+print(findMaxOfArray([1,2,3,4,5,6]))
+print(findMinOfArray([1,2,3,4,5,6]))
